03eb4a4b47275f8a9b2cf91c8ec80b5a5b96bfc6
Andi Susanto <susanto.andi11@gmail.com>
2020-10-16 16:39:46 +0700

    added migration and seeder Penulis

---
+++ database/seeds/TabelPenulisSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TabelPenulisSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('penulis')->insert([
+            ['nama_penulis' => 'Andi Susanto', 'divisi' => 'SEO'],
+            ['nama_penulis' => 'Krisnanto', 'divisi' => 'SEO'],
+            ['nama_penulis' => 'Fandy', 'divisi' => 'SEO']
+        ]);
+    }
+}
